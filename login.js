eac1d1aefd82df05960a007456fd13e885d730da
MohamedHesham45 <mohmedadm733@gmail.com>
2024-06-26 22:52:44 +0300

    Add login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        localStorage.setItem('username', username);
+        window.location.href = 'home.html';
+    });
+});
